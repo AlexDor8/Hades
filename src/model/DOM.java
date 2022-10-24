f3c1bba06cf384040f330fb9a8e56dfcf2b39567
AlexDor8 <st2022273@365.stucom.com>
2022-10-24 19:24:32 +0200

    generarDOM

--- src/model/DOM.java
+++ src/model/DOM.java
@@ -1,5 +1,40 @@
 package model;
 
-public class DOM {
+import java.util.ArrayList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class DOM {
+	private Document document;
+
+	public DOM() {
+		DocumentBuilderFactory factoria = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder builder = factoria.newDocumentBuilder();
+			document = builder.newDocument();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void generarDocument() {
+		Element pantallas = document.createElement("pantallas");
+		document.appendChild(pantallas);
+		
+		JugadoresHandler jugadoresHandler = new JugadoresHandler();
+		
+		ArrayList<Jugador> jugadores = jugadoresHandler.getJugadores();
+		
+		for (int i = 0;i <jugadores.size();i++) {
+			Element pantalla = document.createElement("pantalla");
+			pantallas.appendChild(pantalla);
+		}
+	}
+	
+	
 }
